8bcac3ea47fe0cac3d5a4228d299093fe3c695b5
sungen <sungentim@gmail.com>
2016-04-29 14:22:13 +0800

    rxjava+retorift+dagger模仿写的daily日报

--- daggerdemo/src/main/java/com/tim/daggerdemo/MainActivity.java
+++ daggerdemo/src/main/java/com/tim/daggerdemo/MainActivity.java
@@ -1,17 +1,20 @@
 package com.tim.daggerdemo;
 
+import android.databinding.DataBindingUtil;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.tim.daggerdemo.databinding.ActivityMainBinding;
 import com.tim.daggerdemo.module.ActivityModule;
 import com.tim.daggerdemo.module.ShoppingCartModel;
-import com.tim.daggerdemo.module.UserModule;
 
 import javax.inject.Inject;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.text)
@@ -25,11 +28,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         ButterKnife.bind(this);
         component = DaggerActivityComponent.builder().activityModule(new ActivityModule()).build();
-        containerComponent=DaggerContainerComponent.builder().activityComponent(component).build();
+        containerComponent = DaggerContainerComponent.builder().activityComponent(component).build();
         containerComponent.inject(this);
 
-        mText.setText(shoppingCartModel.getName());
+        binding.setCartModel(shoppingCartModel);
+    }
+
+    @OnClick(R.id.changeBtn)
+    public void onClick() {
+        shoppingCartModel.setName("after changer");
     }
 }
--- daggerdemo/src/main/java/com/tim/daggerdemo/module/ShoppingCartModel.java
+++ daggerdemo/src/main/java/com/tim/daggerdemo/module/ShoppingCartModel.java
@@ -1,16 +1,24 @@
 package com.tim.daggerdemo.module;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.android.databinding.library.baseAdapters.BR;
+
 /**
  * Created by gensun on 16/4/29.
  */
-public class ShoppingCartModel {
+public class ShoppingCartModel extends BaseObservable{
     private String name = "tim";
 
+
+    @Bindable
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+        notifyPropertyChanged(BR.name);
     }
 }
